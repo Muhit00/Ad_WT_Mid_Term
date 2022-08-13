3d0e499c15b0b86c6dbd3b8a1d47fe7678acd445
Muhit00 <98248044+Muhit00@users.noreply.github.com>
2022-08-13 10:17:12 +0600

    Add files via upload

---
+++ database/migrations/2022_07_03_204558_create_order_events_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('price');
+            $table->string('startdate');
+            $table->string('enddate');
+            $table->string('deadline');
+            $table->string('shortdesc');
+            $table->string('desc');
+            $table->string('agentId');
+            $table->string('userId');
+            $table->string('eventId');
+            $table->string('order_status')->default('PAID');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_events');
+    }
+};
